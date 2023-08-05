e775253acd730c366b964ca553a1460259532dcd
sonuaggarwal <sa2762@njit.edu>
2023-08-05 14:20:34 -0400

    change2

---
+++ app/components/Layout.tsx
@@ -0,0 +1,16 @@
+export default function PageLayout({ children, bgColor }: {
+  children: React.ReactNode;
+  bgColor: string;
+}) {
+    return (
+        <html lang="en" style={{ backgroundColor: bgColor }}>
+      <head>
+      </head>
+        <body className={'flex-col min-h-screen'}>
+            <div >
+                {children}
+            </div>
+        </body>
+      </html>
+    )
+  }
